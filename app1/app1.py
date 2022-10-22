3d05975760829c520e9bf7f978c09309a24a956d
Mhinkyu <mhinkyuk@gmail.com>
2022-10-22 23:24:06 +0300

    latest changes

--- app1/app1.py
+++ app1/app1.py
@@ -1,12 +1,12 @@
 from flask import request, Flask
-import json
 
 app1 = Flask(__name__)
+
+
 @app1.route('/')
-
 def hello_world():
-    return "Hello World!"
+    return "Hello World!, This is app1"
 
-if __name__=="__main__":
-    app1.run(debuf = True, host = '0.0.0.0')
+if __name__ == "__main__":
+    app1.run(debug = True, host = '0.0.0.0')
 
--- app2/app2.py
+++ app2/app2.py
@@ -1,11 +1,10 @@
 from flask import request, Flask
-import json
 
 app2 = Flask(__name__)
-@app2.route('/')
 
+@app2.route('/')
 def hello_world():
     return "Hello World!, This is app2"
 
-if __name__=="__main__":
-    app2.run(debuf = True, host = '0.0.0.0')
+if __name__ == "__main__":
+    app2.run(debug = True, host = '0.0.0.0')
